397d5a080a2e85e1ad7c90c5a0c29965430ecaed
Karolis Černiauskis <kcerniauskis@gmail.com>
2022-02-21 14:05:24 +0200

    CHANGE: redundant variable in bootstrap.php, redundant PHP extension ext-http

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -8,6 +8,5 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $containerBuilder = new ContainerBuilder();
 $containerBuilder->addDefinitions(__DIR__ . '/config.php');
-$container = $containerBuilder->build();
 
-return $container;
+return $containerBuilder->build();
